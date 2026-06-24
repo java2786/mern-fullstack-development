0f9c9278279b54cf3132a732ffd3d19f153d93dd
Arun Kumar <java.kumar.arun@gmail.com>
2026-06-24 16:49:26 +0530

    auth

--- NODEJS/server/server_2_mongodb/config/seed_jobs_data.js
+++ NODEJS/server/server_2_mongodb/config/seed_jobs_data.js
@@ -1,15 +1,4 @@
-const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-const Job = require("../models/Job");
-const seed_jobs_data = require("./seed_jobs_data")
-dotenv.config()
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(async () => {
-        console.log('Connected!')
-
-const seed_data = seed_jobs_data;
-const seedJobs = [  
+const seed_jobs_data = [  
   {  
     title: "Backend Developer",  
     company: "SwiftKart",  
@@ -37,18 +26,6 @@ const seedJobs = [
     experienceRequired: 0,  
     description: "Analyze policy and claims data to support regional decisions."  
   }  
-]; 
+];  
 
-        await Job.deleteMany();
-        await Job.insertMany(seed_jobs_data);
-        console.log("Database seeded with sample job");
-
-        process.exit(1);
-        
-    })
-    .catch(function (e) {
-        console.log("mongodb connection error")
-        console.log(e)
-
-        process.exit(1);
-    })
+module.exports = seed_jobs_data
